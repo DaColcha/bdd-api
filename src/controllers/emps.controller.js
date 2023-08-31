9a93142952deaa50a6db586911f8569592e17400
DaColcha <da.colcha@hotmail.com>
2023-08-31 07:56:34 -0500

    Ejemplo crud

--- src/controllers/emps.controller.js
+++ src/controllers/emps.controller.js
@@ -1,9 +1,41 @@
 import getConnection from '../database.js'
 
-const getEmps = async (req, res) => {    
+export const getEmps = async (req, res) => {    
     const pool = await getConnection()
     const result = await pool.request().query('SELECT * FROM Emps')
     res.json(result.recordset)
 }
 
-export default getEmps
+// export const createUser = async (req: Request, res: Response): Promise<Response>=> {
+//     const {name, email} = req.body;
+//     const response: QueryResult = await pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email]);
+  
+//     return res.json({
+//       message: 'User Created successfully',
+//       body: {
+//         user: {
+//           name,
+//           email
+//         }
+//       }
+//     })
+  
+//   }
+  
+//   export const updateUser = async (req: Request, res: Response): Promise<Response>=> {
+//     const id: number = parseInt(req.params.id);
+//     const {name, email} = req.body;
+  
+//     const response: QueryResult = await pool.query('UPDATE users set name= $1, email=$2 WHERE id = $3', [name, email, id]);
+  
+//     return res.json(`User ${id} updated Successfully`)
+//   }
+  
+//   export const deleteUser = async (req: Request, res: Response): Promise<Response>=> {
+//     const id: number = parseInt(req.params.id);
+  
+//       const response: QueryResult = await pool.query(
+//                         'DELETE FROM users WHERE id = $1', [id]);
+//       return res.json(`User ${id} deleted Successfully`);
+  
+//   }
