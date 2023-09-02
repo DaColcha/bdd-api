a4019b91758ea1bd3f3b547bb08cbb05be3ff9af
Martín Mendieta <martin.mendieta@epn.edu.ec>
2023-09-02 17:42:07 -0500

    modify empleado.controller.js to post, put, delete correctly

--- src/controllers/empleado.controller.js
+++ src/controllers/empleado.controller.js
@@ -1,44 +1,64 @@
 import getConnection from '../database.js'
+import sql from 'mssql'
 
-export const getEmpleado = async (req, res) => {    
-    const pool = await getConnection()
-    const result = await pool.request()
-            .query('SELECT cod_empleado, nombre, salario, cargo, cod_agencia, ciudad FROM Empleado')
-    return res.json(result.recordset)
+export const getEmpleado = async (req, res) => {
+  const pool = await getConnection()
+  const result = await pool.request()
+    .query('SELECT cod_empleado, nombre, salario, cargo, cod_agencia, ciudad FROM Empleado')
+  return res.json(result.recordset)
 }
 
-export const createEmpleado = async (req, res) => {  
-    const {cod_empleado, nombre, salario, cargo, cod_agencia, ciudad} = req.body;
-    const pool = await getConnection()
-    const result = await pool
-            .query('INSERT INTO Empleado (cod_empleado, nombre, salario, cargo, cod_agencia, ciudad) VALUES ($1, $2, $3, $4, $5, $6)', [cod_empleado, nombre, salario, cargo, cod_agencia, ciudad]);
-  
-    return res.json({
-      message: 'Employee Created successfully',
-      body: {
-        user: {
-            cod_empleado, nombre, salario, cargo, cod_agencia, ciudad
-        }
-      }
-    })
-  
-  }
-  
-  export const updateEmpleado = async (req, res) => {  
-    const {cod_empleado, nombre, salario, cargo, cod_agencia, ciudad} = req.body;
-    const pool = await getConnection()
+export const createEmpleado = async (req, res) => {
+  const { cod_empleado, nombre, salario, cargo, cod_agencia, ciudad } = req.body;
 
-    const result= await pool.query('UPDATE Empleado set nombre=$1, salario=$2, cargo=$3 WHERE (cod_empleado = $4 AND cod_agencia = $5 AND ciudad = $6)', [ nombre, salario, cargo, cod_empleado, cod_agencia, ciudad ]);
-  
-    return res.json(`Employee ${cc} updated Successfully`)
-  }
-  
+  const pool = await getConnection()
+  await pool
+    .request()
+    .input('cod_empleado', sql.Int, cod_empleado)
+    .input('nombre', sql.VarChar, nombre)
+    .input('salario', sql.Money, salario)
+    .input('cargo', sql.VarChar, cargo)
+    .input('cod_agencia', sql.Int, cod_agencia)
+    .input('ciudad', sql.VarChar, ciudad)
+    .query('begin distributed transaction INSERT INTO Empleado (cod_empleado, nombre, salario, cargo, cod_agencia, ciudad) VALUES (@cod_empleado, @nombre, @salario, @cargo, @cod_agencia, @ciudad) commit');
 
-  export const deleteEmpleado = async (req, res)=> {
-    const {cod_empleado,cod_agencia, ciudad} = req.body;
-    const pool = await getConnection()
-    const result = await pool.query(
-                        'DELETE FROM Empleado WHERE (cod_empleado = $1 AND cod_agencia = $2 AND ciudad = $3)', [cod_empleado,cod_agencia, ciudad]);
+  return res.json({
+    message: 'Employee Created successfully',
+    body: { cod_empleado, nombre, salario, cargo, cod_agencia, ciudad }
+  })
+}
 
-    return res.json(`Employee ${cod_empleado}-${cod_agencia} deleted Successfully`);
-  }
+export const updateEmpleado = async (req, res) => {
+  const { cod_empleado, nombre, salario, cargo, cod_agencia, ciudad } = req.body;
+  
+  const pool = await getConnection()
+  await pool
+    .request()
+    .input('cod_empleado', sql.Int, cod_empleado)
+    .input('nombre', sql.VarChar, nombre)
+    .input('salario', sql.Money, salario)
+    .input('cargo', sql.VarChar, cargo)
+    .input('cod_agencia', sql.Int, cod_agencia)
+    .input('ciudad', sql.VarChar, ciudad)
+    .query('begin distributed transaction UPDATE Empleado set nombre=@nombre, salario=@salario, cargo=@cargo WHERE (cod_empleado=@cod_empleado AND cod_agencia=@cod_agencia AND ciudad=@ciudad) commit');
+
+  return res.json({
+    message: `Employee ${nombre} updated Successfully`,
+    body: {cod_empleado, nombre, salario, cargo, cod_agencia, ciudad}
+  })
+}
+
+
+export const deleteEmpleado = async (req, res) => {
+  const { cod_empleado, cod_agencia, ciudad } = req.body;
+  
+  const pool = await getConnection()
+  await pool
+    .request()
+    .input('cod_empleado', sql.Int, cod_empleado)
+    .input('cod_agencia', sql.Int, cod_agencia)
+    .input('ciudad', sql.VarChar, ciudad)
+    .query('begin distributed transaction DELETE FROM Empleado WHERE (cod_empleado=@cod_empleado AND cod_agencia=@cod_agencia AND ciudad=@ciudad) commit');
+  
+  return res.json(`Employee ${cod_empleado}-${cod_agencia} deleted Successfully`);
+}
