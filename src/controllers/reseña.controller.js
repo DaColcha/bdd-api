e38460b6104d804fedb002a9394d6840956ad25d
Martín Mendieta <martin.mendieta@epn.edu.ec>
2023-09-02 12:30:27 -0500

    new way to control post put delete

--- src/controllers/reseña.controller.js
+++ src/controllers/reseña.controller.js
@@ -1,4 +1,5 @@
 import getConnection from '../database.js'
+import sql from 'mssql'
 
 export const getReseña = async (req, res) => {    
     const pool = await getConnection()
@@ -8,17 +9,37 @@ export const getReseña = async (req, res) => {
 }
 
 export const createReseña = async (req, res) => {  
-    const {num_reseña,cod_pelicula, cc_socio, descripcion, calificacion} = req.body;
-    const pool = await getConnection()
-    const result = await pool
-            .query('INSERT INTO Reseña (num_reseña, cod_pelicula, cc_socio, descripcion, calificacion, fecha) VALUES ($1, $2, $3, $4,$5, CONVERT(Date, GETDATE()))', [num_reseña,cod_pelicula, cc_socio, descripcion, calificacion]);
+  const {num_reseña,cod_pelicula, cc_socio, descripcion, calificacion} = req.body;
+  const pool = await getConnection()
+
+  await pool
+    .request()
+    .input('num_reseña', sql.Int, num_reseña)
+    .input('cod_pelicula', sql.Char, cod_pelicula)
+    .input('cc_socio', sql.Char, cc_socio)
+    .input('descripcion', sql.VarChar, descripcion)
+    .input('calificacion', sql.Int, calificacion)
+    .query('INSERT INTO Reseña (num_reseña, cod_pelicula, cc_socio, descripcion, calificacion, fecha) VALUES (@num_reseña, @cod_pelicula, @cc_socio, @descripcion, @calificacion, CONVERT(Date, GETDATE()))');
+
+  return res.json({
+    message: 'Reseña Created successfully',
+    body: {num_reseña,cod_pelicula, cc_socio, descripcion, calificacion}
+  })
+
+}
+
+// export const createReseña = async (req, res) => {  
+//     const {num_reseña,cod_pelicula, cc_socio, descripcion, calificacion} = req.body;
+//     const pool = await getConnection()
+//     const result = await pool
+//             .query('INSERT INTO Reseña (num_reseña, cod_pelicula, cc_socio, descripcion, calificacion, fecha) VALUES ($1, $2, $3, $4,$5, CONVERT(Date, GETDATE()))', [num_reseña,cod_pelicula, cc_socio, descripcion, calificacion]);
   
-    return res.json({
-      message: 'Reseña Created successfully',
-      body: result
-    })
+//     return res.json({
+//       message: 'Reseña Created successfully',
+//       body: result
+//     })
   
-  }
+//   }
   
   export const updateReseña = async (req, res) => {  
     const {num, desc, calif} = req.body;
@@ -32,9 +53,9 @@ export const createReseña = async (req, res) => {
 
   export const deleteReseña = async (req, res)=> {
     const num= parseInt(req.params.num);
-  
+    const pool = await getConnection()
       const response = await pool.query(
-                        'DELETE FROM Reseña WHERE num = $1', [num]);
+                        'DELETE FROM Reseña WHERE num_reseña = $1', [num]);
 
       return res.json(`Reseña ${num} deleted Successfully`);
   
