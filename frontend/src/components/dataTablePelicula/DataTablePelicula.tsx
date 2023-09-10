a19e8bbaa2e88eb2f86d7b83c5d0c10ca6eff923
Jorge Dueñas <jorge.duenas@epn.edu.ec>
2023-09-10 15:34:23 -0500

    Peliucla

---
+++ frontend/src/components/dataTablePelicula/DataTablePelicula.tsx
@@ -0,0 +1,80 @@
+import { DataGrid, GridColDef, GridToolbar } from "@mui/x-data-grid";
+import "./dataTablePelicula.scss";
+//import { Link } from "react-router-dom";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+// import ModifyAlquiler from "../modify/ModifyPelicula";
+
+type Props = {
+  columns: GridColDef[];
+  rows: object[];
+  slug: string;
+  onEditClick: (item: any) => void;
+};
+
+const DataTablePelicula = (props: Props) => {
+
+
+  const queryClient = useQueryClient();
+  const mutation = useMutation({
+    mutationFn: (id: string) => {
+      const [cod_pelicula] = id.split("-");
+      return fetch(`http://localhost:4000/glob-guster/${props.slug}/${cod_pelicula}`, {
+        method: "delete",
+      });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries([`all${props.slug}`]);
+    },
+  });
+
+  const handleDelete = (id: number) => {
+    //delete the item
+    let ID: string;
+    ID = `${id}`;
+    mutation.mutate(ID);
+    window.location.reload();
+  };
+
+  const actionColumn: GridColDef = {
+    field: "actionD",
+    headerName: "Delete",
+    width: 200,
+    renderCell: (params) => {
+      return (
+        <div className="action">
+          <div className="delete" onClick={() => handleDelete(params.row.id)}>
+            <img src="/delete.svg" alt="" />
+          </div>
+        </div>
+      );
+    },
+  };
+
+   return (
+    <div className="dataTable">
+      <DataGrid
+        className="dataGrid"
+        rows={props.rows}
+        columns={[
+          ...props.columns,
+          {
+            field: "action",
+            headerName: "Update",
+            width: 200,
+            renderCell: (params) => (
+              <div className="action">
+                <div className='modify' onClick={() => props.onEditClick(params.row)}>
+                  <img src="/view.svg" alt="" />
+                </div>
+              </div>
+
+            ),
+          }, actionColumn
+        ]}
+      />
+    </div>
+  );
+};
+
+export default DataTablePelicula;
---
+++ frontend/src/pages/peliculas/Peliculas.tsx
@@ -0,0 +1,103 @@
+import { GridColDef } from "@mui/x-data-grid";
+// import DataTableAlquiler from "../../components/dataTableAlquiler/DataTableAlquiler";
+import "./alquileres.scss";
+import { useState } from "react";
+import Add from "../../components/add/Add";
+
+import { useQuery } from "@tanstack/react-query";
+import Update from "../../components/update/Update";
+import DataTableAlquiler from "../../components/dataTablePelicula/DataTablePelicula";
+
+const columns: GridColDef[] = [
+  {
+    field: "cod_pelicula",
+    type: "number",
+    headerName: "Cod. Pelicula",
+    width: 75,
+  },
+  {
+    field: "titulo",
+    type: "string",
+    headerName: "Título",
+    width: 150,
+  },
+  {
+    field: "cod_dir",
+    type: "number",
+    headerName: "Cod. Director",
+    width: 150,
+  },
+  {
+    field: "nacionalidad",
+    type: "string",
+    headerName: "Nacionalidad",
+    width: 150,
+  },
+  {
+    field: "productora",
+    type: "string",
+    headerName: "Productora",
+    width: 150,
+  },
+  {
+    field: "fecha_estreno",
+    type: "string",
+    headerName: "Fecha Estreno",
+    width: 150,
+  },
+];
+
+const Peliculas = () => {
+  const [openAddModal, setOpenAddModal] = useState(false);
+  const [isUpdateModalOpen, setUpdateModalOpen] = useState(false);
+  const [selectedItem, setSelectedItem] = useState<any>(null); // Almacena el elemento seleccionado para actualizar
+
+  const { isLoading, data } = useQuery({
+    queryKey: ["allpeliculas"],
+    queryFn: () =>
+      fetch("http://localhost:4000/glob-guster/pelicula").then((res) => res.json()),
+  });
+
+  const handleEditClick = (item: any) => {
+    // Abre el modal de actualización y pasa el elemento seleccionado
+    setSelectedItem(item);
+    setUpdateModalOpen(true);
+  };
+  
+  return (
+    <div className="peliculas">
+      <div className="peliculas__header">
+        <h1>Peliculas</h1>
+        <button onClick={() => setOpenAddModal(true)}>Agregar</button>
+      </div>
+      <div className="peliculas__table">
+        <DataTableAlquiler
+          columns={[...columns, { ...actionColumn, renderCell: (params) => {
+            return (
+              <div className="action">
+                <div className="delete" onClick={() => handleEditClick(params.row)}>
+                  <img src="/delete.svg" alt="" />
+                </div>
+              </div>
+            );
+          }}]}
+          rows={data}
+          slug="pelicula"
+        />
+      </div>
+      <Add
+        open={openAddModal}
+        setOpen={setOpenAddModal}
+        slug="pelicula"
+      />
+      <Update
+        open={isUpdateModalOpen}
+        setOpen={setUpdateModalOpen}
+        slug="pelicula"
+        item={selectedItem}
+      />
+    </div>
+  );
+};
+
+export default Peliculas;
